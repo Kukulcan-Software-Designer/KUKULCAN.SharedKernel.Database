94b1cf20aa6159695bb6ba5ab7afef6b5728d0e7
Juan Manuel Pardo García <jpardo.kukulcan@gmail.com>
2026-08-23 18:18:26 +0200

    test: cover DbContext configuration and combined filters

--- Tests/KUKULCAN.SharedKernel.Database.Tests/KukulcanDbContextBaseTests.cs
+++ Tests/KUKULCAN.SharedKernel.Database.Tests/KukulcanDbContextBaseTests.cs
@@ -1,4 +1,5 @@
 using KUKULCAN.SharedKernel.Database.Tests.TestInfrastructure.internals;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace KUKULCAN.SharedKernel.Database.Tests;
@@ -108,6 +109,32 @@ public sealed class KukulcanDbContextBaseTests
     }
 
     [Test]
+    public async Task TenantAndSoftDeleteFilters_ShouldApplyTogether()
+    {
+        Guid currentTenant = Guid.NewGuid();
+        Guid otherTenant = Guid.NewGuid();
+        await using var context = new CombinedFilterTestDbContext(
+            Options.Create(new KukulcanDatabaseOptions()),
+            new TestTenantContext(currentTenant),
+            new TestClock(DateTimeOffset.UtcNow),
+            Mock.Of<IDomainEventDispatcher>());
+
+        context.Entities.AddRange(
+            new CombinedFilterEntity { TenantId = currentTenant, IsDeleted = false },
+            new CombinedFilterEntity { TenantId = currentTenant, IsDeleted = true },
+            new CombinedFilterEntity { TenantId = otherTenant, IsDeleted = false },
+            new CombinedFilterEntity { TenantId = otherTenant, IsDeleted = true });
+
+        await context.SaveChangesAsync();
+
+        List<CombinedFilterEntity> visible = await context.Entities.ToListAsync();
+
+        Assert.That(visible, Has.Count.EqualTo(1));
+        Assert.That(visible[0].TenantId, Is.EqualTo(currentTenant));
+        Assert.That(visible[0].IsDeleted, Is.False);
+    }
+
+    [Test]
     public async Task SoftDeleteFilter_ShouldHideDeletedEntities()
     {
         await using TestDbContext context = DatabaseTestContextFactory.Create().Context;
@@ -121,5 +148,144 @@ public sealed class KukulcanDbContextBaseTests
 
         Assert.That(result, Has.Count.EqualTo(1));
         Assert.That(result[0], Is.SameAs(visible));
+    }
+
+    [Test]
+    public async Task OnConfiguring_WhenOptionsAreAlreadyConfigured_ShouldNotReconfigureProvider()
+    {
+        var builder = new DbContextOptionsBuilder<PreconfiguredDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString());
+        var options = Options.Create(new KukulcanDatabaseOptions());
+
+        await using var context = new PreconfiguredDbContext(
+            options,
+            new TestTenantContext(Guid.NewGuid()),
+            new TestClock(DateTimeOffset.UtcNow),
+            Mock.Of<IDomainEventDispatcher>(),
+            builder.Options);
+
+        await context.Database.EnsureCreatedAsync();
+
+        Assert.That(context.ConfigureProviderCalled, Is.False);
+    }
+
+    [Test]
+    public void OnConfiguring_ShouldEnableSensitiveLoggingAndDetailedErrors()
+    {
+        var options = Options.Create(new KukulcanDatabaseOptions
+        {
+            EnableSensitiveDataLogging = true,
+            EnableDetailedErrors = true
+        });
+
+        using var context = new ConfigurableDbContext(
+            options,
+            new TestTenantContext(Guid.NewGuid()),
+            new TestClock(DateTimeOffset.UtcNow),
+            Mock.Of<IDomainEventDispatcher>());
+
+        CoreOptionsExtension coreOptions = context
+            .GetService<IDbContextOptions>()
+            .Extensions
+            .OfType<CoreOptionsExtension>()
+            .Single();
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(coreOptions.IsSensitiveDataLoggingEnabled, Is.True);
+            Assert.That(coreOptions.DetailedErrorsEnabled, Is.True);
+        }
+    }
+
+    [Test]
+    public void OnConfiguring_WithUnsupportedProvider_ShouldThrow()
+    {
+        var options = Options.Create(new KukulcanDatabaseOptions
+        {
+            Provider = (DatabaseProvider)999,
+            ConnectionString = "ignored"
+        });
+
+        using var context = new UnsupportedProviderDbContext(
+            options,
+            new TestTenantContext(Guid.NewGuid()),
+            new TestClock(DateTimeOffset.UtcNow),
+            Mock.Of<IDomainEventDispatcher>());
+
+        Assert.That(
+            () => context.Database.EnsureCreated(),
+            Throws.TypeOf<NotSupportedException>()
+                .With.Message.Contains("not supported"));
+    }
+
+    private sealed class ConfigurableDbContext(
+        IOptions<KukulcanDatabaseOptions> options,
+        ITenantContext tenantContext,
+        IClock clock,
+        IDomainEventDispatcher dispatcher)
+        : KukulcanDbContextBase(options, tenantContext, clock, dispatcher)
+    {
+        protected override void ConfigureProvider(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+    }
+
+    private sealed class PreconfiguredDbContext(
+        IOptions<KukulcanDatabaseOptions> options,
+        ITenantContext tenantContext,
+        IClock clock,
+        IDomainEventDispatcher dispatcher,
+        DbContextOptions<PreconfiguredDbContext> dbOptions)
+        : KukulcanDbContextBase(options, tenantContext, clock, dispatcher)
+    {
+        public bool ConfigureProviderCalled { get; private set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => base.OnConfiguring(optionsBuilder);
+
+        protected override void ConfigureProvider(DbContextOptionsBuilder optionsBuilder)
+        {
+            ConfigureProviderCalled = true;
+            throw new AssertionException("ConfigureProvider must not be called for preconfigured options.");
+        }
+
+        public PreconfiguredDbContext(
+            IOptions<KukulcanDatabaseOptions> options,
+            ITenantContext tenantContext,
+            IClock clock,
+            IDomainEventDispatcher dispatcher,
+            DbContextOptions dbOptions)
+            : this(options, tenantContext, clock, dispatcher, (DbContextOptions<PreconfiguredDbContext>)dbOptions)
+        {
+        }
+    }
+
+    private sealed class UnsupportedProviderDbContext(
+        IOptions<KukulcanDatabaseOptions> options,
+        ITenantContext tenantContext,
+        IClock clock,
+        IDomainEventDispatcher dispatcher)
+        : KukulcanDbContextBase(options, tenantContext, clock, dispatcher)
+    {
+    }
+
+    private sealed class CombinedFilterTestDbContext(
+        IOptions<KukulcanDatabaseOptions> options,
+        ITenantContext tenantContext,
+        IClock clock,
+        IDomainEventDispatcher dispatcher)
+        : KukulcanDbContextBase(options, tenantContext, clock, dispatcher)
+    {
+        public DbSet<CombinedFilterEntity> Entities => Set<CombinedFilterEntity>();
+
+        protected override void ConfigureProvider(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+    }
+
+    private sealed class CombinedFilterEntity : ISoftDelete
+    {
+        public int Id { get; set; }
+        public Guid TenantId { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTimeOffset? DeletedOn { get; set; }
     }
 }
