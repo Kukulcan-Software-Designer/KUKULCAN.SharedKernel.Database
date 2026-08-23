31a25fc8a870930913d558409d52f719df3ee7a9
Juan Manuel Pardo García <jpardo.kukulcan@gmail.com>
2026-08-23 18:28:34 +0200

    test: fix DI setup for interceptor test

--- Tests/KUKULCAN.SharedKernel.Database.Tests/Extensions/ServiceCollectionExtensionsTests.cs
+++ Tests/KUKULCAN.SharedKernel.Database.Tests/Extensions/ServiceCollectionExtensionsTests.cs
@@ -97,6 +97,10 @@ public sealed class ServiceCollectionExtensionsTests
             .Build();
 
         services.AddKukulcanDbContext<TestDbContext>(configuration);
+        services.AddSingleton<ITenantContext>(new TestTenantContext(Guid.NewGuid()));
+        services.AddSingleton<IClock>(new TestClock(DateTimeOffset.UtcNow));
+        services.AddSingleton<IDomainEventDispatcher>(Mock.Of<IDomainEventDispatcher>());
+
         using ServiceProvider provider = services.BuildServiceProvider();
         using TestDbContext context = provider.GetRequiredService<TestDbContext>();
 
