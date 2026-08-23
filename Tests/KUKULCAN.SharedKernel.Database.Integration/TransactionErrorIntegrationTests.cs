fb697c9c499d2e1ae922263f45c95f7485caa18b
Juan Manuel Pardo García <jpardo.kukulcan@gmail.com>
2026-08-24 00:15:00 +0200

    test: cover PostgreSQL transaction constraint failure and rollback

---
+++ Tests/KUKULCAN.SharedKernel.Database.Integration/TransactionErrorIntegrationTests.cs
@@ -0,0 +1,56 @@
+using KUKULCAN.SharedKernel.Database.Abstractions;
+using KUKULCAN.SharedKernel.Database.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using NUnit.Framework;
+
+namespace KUKULCAN.SharedKernel.Database.Integration;
+
+[TestFixture]
+[NonParallelizable]
+public sealed class TransactionErrorIntegrationTests
+{
+    [Test]
+    public async Task UnitOfWork_FailedCommit_ShouldRollbackDatabaseTransactionAfterPostgreSqlConstraintViolation()
+    {
+        Guid tenantId = Guid.NewGuid();
+        await using PostgreSqlDatabaseIntegrationTests.IntegrationDbContext context =
+            await IntegrationTestDatabase.CreateContextAsync(tenantId);
+
+        await context.Database.ExecuteSqlRawAsync(
+            "CREATE UNIQUE INDEX IF NOT EXISTS \"UX_IntegrationEntity_TenantId_Name\" " +
+            "ON \"IntegrationEntity\" (\"TenantId\", \"Name\");");
+
+        var unitOfWork = new UnitOfWork<PostgreSqlDatabaseIntegrationTests.IntegrationDbContext>(context);
+
+        try
+        {
+            await unitOfWork.BeginTransactionAsync();
+
+            context.Entities.Add(new PostgreSqlDatabaseIntegrationTests.IntegrationEntity
+            {
+                TenantId = tenantId,
+                Name = "transaction-error"
+            });
+            await context.SaveChangesAsync();
+
+            context.Entities.Add(new PostgreSqlDatabaseIntegrationTests.IntegrationEntity
+            {
+                TenantId = tenantId,
+                Name = "transaction-error"
+            });
+
+            Assert.ThrowsAsync<DbUpdateException>(async () => await context.SaveChangesAsync());
+            await unitOfWork.RollbackTransactionAsync();
+
+            Assert.That(
+                await context.Entities.IgnoreQueryFilters()
+                    .CountAsync(x => x.TenantId == tenantId && x.Name == "transaction-error"),
+                Is.EqualTo(0));
+        }
+        finally
+        {
+            await unitOfWork.DisposeAsync();
+        }
+    }
+}
