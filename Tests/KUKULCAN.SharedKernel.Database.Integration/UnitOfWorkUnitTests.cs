b12d61f88ca3821c76c059040ce6c782ee6e0d8d
Juan Manuel Pardo García <jpardo.kukulcan@gmail.com>
2026-08-24 14:04:45 +0200

    test: add unit coverage for UnitOfWork null context contract

---
+++ Tests/KUKULCAN.SharedKernel.Database.Integration/UnitOfWorkUnitTests.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+
+namespace KUKULCAN.SharedKernel.Database.Integration;
+
+[TestFixture]
+public sealed class UnitOfWorkUnitTests
+{
+    [Test]
+    public void Constructor_ShouldRejectNullContext()
+    {
+        ArgumentNullException exception = Assert.Throws<ArgumentNullException>(() =>
+            new UnitOfWork<UnitTestDbContext>(null!))!;
+
+        Assert.That(exception.ParamName, Is.EqualTo("context"));
+    }
+
+    private sealed class UnitTestDbContext : KukulcanDbContextBase
+    {
+        public UnitTestDbContext()
+            : base(
+                Options.Create(new KukulcanDatabaseOptions
+                {
+                    Provider = DatabaseProvider.PostgresSql,
+                    ConnectionString = "Host=unit-test;Database=unit-test;Username=unit-test;Password=unit-test"
+                }),
+                Mock.Of<ITenantContext>(),
+                Mock.Of<IClock>(),
+                Mock.Of<IDomainEventDispatcher>())
+        {
+        }
+    }
+}
