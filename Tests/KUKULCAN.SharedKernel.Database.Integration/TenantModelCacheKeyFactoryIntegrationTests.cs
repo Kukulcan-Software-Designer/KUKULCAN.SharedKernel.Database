ca21ad0334e8f5bee80fce97dd8ebbfd6a870b5a
Juan Manuel Pardo García <jpardo.kukulcan@gmail.com>
2026-08-24 13:07:06 +0200

    test: cover non-Kukulcan tenant cache key branch

---
+++ Tests/KUKULCAN.SharedKernel.Database.Integration/TenantModelCacheKeyFactoryIntegrationTests.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace KUKULCAN.SharedKernel.Database.Integration;
+
+public sealed class TenantModelCacheKeyFactoryIntegrationTests
+{
+    [Test]
+    public void Create_NonKukulcanDbContext_UsesNullTenantId()
+    {
+        using var context = new DbContext(new DbContextOptionsBuilder<DbContext>().Options);
+        var factory = new TenantModelCacheKeyFactory();
+
+        object key = factory.Create(context, designTime: false);
+
+        Assert.That(key, Is.TypeOf<ValueTuple<Type, Guid?, bool>>());
+
+        (Type contextType, Guid? tenantId, bool designTime) =
+            ((Type, Guid?, bool))key;
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(contextType, Is.EqualTo(typeof(DbContext)));
+            Assert.That(tenantId, Is.Null);
+            Assert.That(designTime, Is.False);
+        }
+    }
+}
