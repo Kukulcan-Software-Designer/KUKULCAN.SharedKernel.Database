dfa0484a21650756d09509dcc0b8ffe54bff131a
Juan Manuel Pardo García <jpardo.kukulcan@gmail.com>
2026-08-26 21:44:55 +0200

    Correct deleting files and methodes fron testing redundants

--- Tests/KUKULCAN.SharedKernel.Database.Tests/KukulcanDbContextProviderConfigurationTests.cs
+++ Tests/KUKULCAN.SharedKernel.Database.Tests/KukulcanDbContextProviderConfigurationTests.cs
@@ -7,35 +7,6 @@ namespace KUKULCAN.SharedKernel.Database.Tests;
 public sealed class KukulcanDbContextProviderConfigurationTests
 {
     [Test]
-    public void ConfigureProvider_WithSqlServer_ShouldConfigureSqlServer()
-    {
-        using var context = new ProviderTestDbContext(
-            Options.Create(new KukulcanDatabaseOptions
-            {
-                Provider = DatabaseProvider.SqlServer,
-                ConnectionString = "Server=localhost;Database=KukulcanTests;Integrated Security=True;",
-                CommandTimeoutSeconds = 45,
-                Retry = new KukulcanDatabaseOptions.RetryOptions
-                {
-                    Enabled = true,
-                    MaxRetryCount = 7,
-                    MaxRetryDelaySeconds = 12
-                }
-            }),
-            new TestTenantContext(Guid.NewGuid()),
-            new TestClock(DateTimeOffset.UtcNow),
-            Mock.Of<IDomainEventDispatcher>());
-
-        using (Assert.EnterMultipleScope())
-        {
-            Assert.That(context.Database.ProviderName, Is.EqualTo("Microsoft.EntityFrameworkCore.SqlServer"));
-            Assert.That(context.Database.GetCommandTimeout(), Is.EqualTo(45));
-            Assert.That(context.Database.CreateExecutionStrategy().GetType().Name,
-                Is.EqualTo("SqlServerRetryingExecutionStrategy"));
-        }
-    }
-
-    [Test]
     public void ConfigureProvider_WithPostgresSql_ShouldConfigurePostgreSql()
     {
         using var context = new ProviderTestDbContext(
@@ -103,39 +74,6 @@ public sealed class KukulcanDbContextProviderConfigurationTests
 
         Assert.That(exception.InnerException, Is.TypeOf<NotSupportedException>());
         Assert.That(exception.InnerException!.Message, Does.Contain("Failed to configure provider"));
-    }
-
-    [Test]
-    public void LoadProviderExtensionType_WhenExpectedTypeIsMissing_ShouldRejectProvider()
-    {
-        MethodInfo method = typeof(KukulcanDbContextBase).GetMethod(
-            "LoadProviderExtensionType", BindingFlags.Static | BindingFlags.NonPublic)!;
-
-        TargetInvocationException exception = Assert.Throws<TargetInvocationException>(() =>
-            method.Invoke(null,
-                ["KUKULCAN.SharedKernel.Database.Tests.TypeThatDoesNotExist", "Microsoft.EntityFrameworkCore.SqlServer"]))!;
-
-        Assert.That(exception.InnerException, Is.TypeOf<NotSupportedException>());
-        Assert.That(exception.InnerException!.Message, Does.Contain("does not expose the expected provider extension type"));
-    }
-
-    [Test]
-    public void ConfigureProvider_WhenRetryIsDisabled_ShouldNotEnableRetryingStrategy()
-    {
-        using var context = new ProviderTestDbContext(
-            Options.Create(new KukulcanDatabaseOptions
-            {
-                Provider = DatabaseProvider.SqlServer,
-                ConnectionString = "Server=localhost;Database=KukulcanTests;Integrated Security=True;",
-                Retry = new KukulcanDatabaseOptions.RetryOptions { Enabled = false }
-            }),
-            new TestTenantContext(Guid.NewGuid()),
-            new TestClock(DateTimeOffset.UtcNow),
-            Mock.Of<IDomainEventDispatcher>());
-
-        Assert.That(
-            context.Database.CreateExecutionStrategy().GetType().Name,
-            Is.EqualTo("SqlServerExecutionStrategy"));
     }
 
     private sealed class ProviderTestDbContext(
