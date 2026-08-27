c3b42b09cbdf4ebcc8ad174814e0055220404c8f
Juan Manuel Pardo García <jpardo.kukulcan@gmail.com>
2026-08-27 13:21:04 +0200

    test: complete KukulcanDbContextBase coverage

--- Tests/KUKULCAN.SharedKernel.Database.Tests/KukulcanDbContextBaseUnitTests.cs
+++ Tests/KUKULCAN.SharedKernel.Database.Tests/KukulcanDbContextBaseUnitTests.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using KUKULCAN.SharedKernel.Database.Configuration;
 using NUnit.Framework;
 
@@ -93,12 +94,188 @@ public sealed class KukulcanDbContextBaseUnitTests
         Assert.That(exception.Message, Does.Contain("not supported"));
     }
 
+    [Test]
+    public void ConfigureProvider_WithRetryDisabled_ShouldNotEnableProviderRetry()
+    {
+        var options = Options.Create(new KukulcanDatabaseOptions
+        {
+            Provider = DatabaseProvider.PostgresSql,
+            ConnectionString = "Host=localhost;Database=KukulcanTests;Username=test;Password=test",
+            CommandTimeoutSeconds = 31,
+            Retry = new KukulcanDatabaseOptions.RetryOptions
+            {
+                Enabled = false,
+                MaxRetryCount = 7,
+                MaxRetryDelaySeconds = 12
+            }
+        });
+
+        using var context = new UnitTestDbContext(
+            options,
+            Mock.Of<ITenantContext>(),
+            Mock.Of<IClock>(),
+            Mock.Of<IDomainEventDispatcher>());
+
+        var builder = new DbContextOptionsBuilder();
+        context.ConfigureProviderForTest(builder);
+
+        using var configured = new DbContext(builder.Options);
+        Assert.That(configured.Database.ProviderName, Is.EqualTo("Npgsql.EntityFrameworkCore.PostgreSQL"));
+        Assert.That(configured.Database.GetCommandTimeout(), Is.EqualTo(31));
+        Assert.That(configured.Database.CreateExecutionStrategy().GetType().Name,
+            Is.EqualTo("NpgsqlExecutionStrategy"));
+    }
+
+    [TestCase(DatabaseProvider.SqlServer,
+        "Server=localhost;Database=Coverage;Pooling=true;Min Pool Size=2;Max Pool Size=9;")]
+    [TestCase(DatabaseProvider.PostgresSql,
+        "Host=localhost;Database=Coverage;Pooling=true;Minimum Pool Size=2;Maximum Pool Size=9;")]
+    [TestCase(DatabaseProvider.MySql,
+        "Server=localhost;Database=Coverage;Pooling=true;MinimumPoolSize=2;MaximumPoolSize=9;")]
+    public void BuildProviderConnectionString_WithPoolingEnabled_ShouldAppendProviderSpecificKeys(
+        DatabaseProvider provider,
+        string expected)
+    {
+        var pool = new KukulcanDatabaseOptions.PoolOptions
+        {
+            Enabled = true,
+            MinSize = 2,
+            MaxSize = 9
+        };
+
+        string result = InvokePrivate<string>(
+            "BuildProviderConnectionString",
+            provider,
+            "Server=localhost;Database=Coverage;",
+            pool);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [TestCase(DatabaseProvider.SqlServer, "Pooling;Min Pool Size;Max Pool Size")]
+    [TestCase(DatabaseProvider.PostgresSql, "Pooling;Minimum Pool Size;Maximum Pool Size")]
+    [TestCase(DatabaseProvider.MySql, "Pooling;MinimumPoolSize;MaximumPoolSize;MinPoolSize;MaxPoolSize")]
+    public void BuildProviderConnectionString_WithPoolingDisabled_ShouldRemoveProviderSpecificKeys(
+        DatabaseProvider provider,
+        string keys)
+    {
+        var pool = new KukulcanDatabaseOptions.PoolOptions { Enabled = false };
+        string connectionString =
+            "Server=localhost;Database=Coverage;Pooling=true;Min Pool Size=2;Max Pool Size=9;" +
+            "Minimum Pool Size=3;Maximum Pool Size=8;MinimumPoolSize=4;MaximumPoolSize=7;" +
+            "MinPoolSize=5;MaxPoolSize=6;Application Name=Coverage;";
+
+        string result = InvokePrivate<string>(
+            "BuildProviderConnectionString",
+            provider,
+            connectionString,
+            pool);
+
+        Assert.That(result, Does.Contain("Application Name=Coverage"));
+        foreach (string key in keys.Split(';'))
+            Assert.That(result, Does.Not.Contain(key + "="));
+    }
+
+    [Test]
+    public void BuildProviderConnectionString_WithEmptyConnectionString_ShouldReturnOnlyPoolOptions()
+    {
+        var pool = new KukulcanDatabaseOptions.PoolOptions
+        {
+            Enabled = true,
+            MinSize = 1,
+            MaxSize = 4
+        };
+
+        string result = InvokePrivate<string>(
+            "BuildProviderConnectionString",
+            DatabaseProvider.PostgresSql,
+            string.Empty,
+            pool);
+
+        Assert.That(result, Is.EqualTo("Pooling=true;Minimum Pool Size=1;Maximum Pool Size=4"));
+    }
+
+    [Test]
+    public void BuildProviderConnectionString_WithWhitespaceAndDisabledPooling_ShouldPreserveWhitespaceInput()
+    {
+        var pool = new KukulcanDatabaseOptions.PoolOptions { Enabled = false };
+
+        string result = InvokePrivate<string>(
+            "BuildProviderConnectionString",
+            DatabaseProvider.SqlServer,
+            "   ",
+            pool);
+
+        Assert.That(result, Is.EqualTo("   "));
+    }
+
+    [Test]
+    public void BuildProviderConnectionString_WithUnsupportedProvider_ShouldLeaveConnectionStringUnchanged()
+    {
+        var pool = new KukulcanDatabaseOptions.PoolOptions { Enabled = true };
+        const string connectionString = "Server=localhost;Database=Coverage;";
+
+        string result = InvokePrivate<string>(
+            "BuildProviderConnectionString",
+            (DatabaseProvider)999,
+            connectionString,
+            pool);
+
+        Assert.That(result, Is.EqualTo(connectionString));
+    }
+
+    [Test]
+    public void BuildProviderConnectionString_WithUnsupportedProviderAndPoolingDisabled_ShouldLeaveConnectionStringUnchanged()
+    {
+        var pool = new KukulcanDatabaseOptions.PoolOptions { Enabled = false };
+        const string connectionString = "Server=localhost;Database=Coverage;";
+
+        string result = InvokePrivate<string>(
+            "BuildProviderConnectionString",
+            (DatabaseProvider)999,
+            connectionString,
+            pool);
+
+        Assert.That(result, Is.EqualTo(connectionString));
+    }
+
+    [Test]
+    public void AppendConnectionStringOptions_WithWhitespaceConnectionString_ShouldReturnOptionsOnly()
+    {
+        string result = InvokePrivate<string>(
+            "AppendConnectionStringOptions",
+            "  ",
+            "Pooling=true");
+
+        Assert.That(result, Is.EqualTo("Pooling=true"));
+    }
+
+    [Test]
+    public void RemoveConnectionStringKeys_WithEmptyConnectionString_ShouldReturnOriginalValue()
+    {
+        string result = InvokePrivate<string>(
+            "RemoveConnectionStringKeys",
+            "",
+            new[] { "Pooling" });
+
+        Assert.That(result, Is.Empty);
+    }
+
     private static IOptions<KukulcanDatabaseOptions> CreateOptions()
         => Options.Create(new KukulcanDatabaseOptions
         {
             Provider = DatabaseProvider.PostgresSql,
             ConnectionString = "Host=unit-test;Database=unit-test;Username=unit-test;Password=unit-test"
         });
+
+    private static T InvokePrivate<T>(string methodName, params object?[] arguments)
+    {
+        MethodInfo method = typeof(KukulcanDbContextBase).GetMethod(
+            methodName,
+            BindingFlags.NonPublic | BindingFlags.Static)!;
+
+        return (T)method.Invoke(null, arguments)!;
+    }
 
     private sealed class UnitTestDbContext : KukulcanDbContextBase
     {
